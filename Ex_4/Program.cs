0ecc4a48584647cf9213f0afca885a84ce78f872
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-14 22:05:47 +0300

    Выполнено задание про дни недели

---
+++ Ex_4/Program.cs
@@ -0,0 +1,25 @@
+﻿//По заданному номеру дня недели вывести его название
+
+List<string> week = new List<string>();
+{
+    week.Add("Понедельник");
+    week.Add("Вторник");
+    week.Add("Среда");
+    week.Add("Четверг");
+    week.Add("Пятница");
+    week.Add("Суббота");
+    week.Add("Воскресенье");
+}
+
+Console.Write("Введите номер дня недели: ");
+int number_day = Convert.ToInt32(Console.ReadLine());
+
+if (number_day >= 1 && number_day <= 7)
+{
+    Console.Write("Название дня: ");
+    Console.Write(week[number_day-1]);
+}
+else
+{
+    Console.WriteLine("Введен некорректный номер дня недели!");
+}
