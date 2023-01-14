7f50564f3270a201de794be8c1826a0f4061291e
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-14 21:30:21 +0300

    Окончание задачи по проверке первого и второго числа

---
+++ Ex_3/Program.cs
@@ -0,0 +1,15 @@
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == Math.Pow(numberB, 2))
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
