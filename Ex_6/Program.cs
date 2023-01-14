3fecdf3e1db19635506662d61a55f8584ea0afad
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-15 00:06:44 +0300

    Выполнена задача по определению четности числа.

---
+++ Ex_6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Выяснить является ли число чётным
+
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine("Число является четным.");
+}
+else
+{
+    Console.WriteLine("Число является нечетным.");
+}
