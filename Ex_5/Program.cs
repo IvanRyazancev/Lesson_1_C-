0a03b0ba1c972662c8f61d2c080996a1e1fcba63
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-14 22:42:01 +0300

    Выполнено задание по нахождению максимального числа из трех чисел

---
+++ Ex_5/Program.cs
@@ -0,0 +1,31 @@
+﻿//Найти максимальное из трех чисел
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max_number = 0;
+
+if (numberA > numberB)
+{
+    max_number = numberA;    
+}
+else
+{
+    max_number = numberB;
+}
+
+if (max_number < numberC)
+{
+    max_number = numberC;
+    Console.Write("Максимальное число: ");
+    Console.Write(max_number);   
+}
+else
+{
+    Console.Write("Максимальное число: ");
+    Console.Write(max_number);
+}
