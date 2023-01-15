f3978db5ebd1ba1838959b2170701a628c345870
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-15 16:17:44 +0300

    Кратно ли число заданному, если нет, вывести остаток.

---
+++ Ex_12/Program.cs
@@ -0,0 +1,16 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int numberA = 5;
+Console.Write("Введите число от 1 до 99: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Заданное число: " + numberA);
+
+if (numberB % numberA == 0)
+{
+    Console.WriteLine("Введенное число кратно заданному.");
+}
+else
+{
+    int remains = numberB % numberA;
+    Console.WriteLine("Введенное число некратно заданному. Остаток от деления:" + remains);
+}
