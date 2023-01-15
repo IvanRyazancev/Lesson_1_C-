5cb54406d8f1de3679a6c91760c4aaa718e8a907
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-15 13:39:59 +0300

    Выполнена задача поиска максимальной цифры в случайном числе от 10 до 99.

---
+++ Ex_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10,99); 
+string numberA = Convert.ToString(number);
+Console.Write("Сгенерированное число: ");
+Console.WriteLine(numberA);
+
+if (numberA[0] > numberA[1])
+{    
+    Console.WriteLine("Наибольшая цифра, сгенерированного числа:" + numberA[0]);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра, сгенерированного числа:" + numberA[1]);
+}
