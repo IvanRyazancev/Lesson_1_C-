3ae5b3cafed3dc149a58e815788f8ecb3c56fbe4
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-15 14:46:54 +0300

    Удаление второй цифры в трехзначном числе.

---
+++ Ex_11/Program.cs
@@ -0,0 +1,9 @@
+﻿//Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100,999); 
+string numberA = Convert.ToString(number);
+Console.Write("Сгенерированное число: ");
+Console.WriteLine(numberA);
+
+Console.WriteLine("Удаленное число:" + numberA[1]);
+Console.WriteLine("Новое число:" + numberA.Remove(1, 1));
