1bdebb7517b53455a044fcf3900528ccace0955d
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-15 10:47:15 +0300

    Завершение задачи по показу четных чисел от 1 до N

---
+++ Ex_7/Program.cs
@@ -0,0 +1,18 @@
+﻿//Показать четные числа от 1 до N
+
+Console.WriteLine("Введите конечное число последовательности: ");
+int lust_number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count <=lust_number)
+{
+    if (count % 2 == 0)
+    {        
+        Console.WriteLine(count);
+        count += 2;
+    }
+    else
+    {
+        count += 1;
+    }
+}
