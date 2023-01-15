dc2975f316d75e2d66289d9851e450694bf3bfcc
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-15 11:09:12 +0300

    Выполнена задача по выводу последнего символа трехзначного числа.

---
+++ Ex_8/Program.cs
@@ -0,0 +1,6 @@
+﻿//Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число: ");
+string number = Console.ReadLine();
+
+Console.WriteLine("Последнее число: " + number[2]);
