5694f0124bcb45bbcae25b86b0fe6503841407be
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-15 11:14:49 +0300

    Выполнено задание по выводу второго символа в трехзначном числе

---
+++ Ex_9/Program.cs
@@ -0,0 +1,6 @@
+﻿//Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число: ");
+string number = Console.ReadLine();
+
+Console.WriteLine("Второй символ: " + number[1]);
